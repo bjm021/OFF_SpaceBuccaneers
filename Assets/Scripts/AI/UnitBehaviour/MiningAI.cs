846306235bb08b00a41e58b9b7bb417462903d35
Benjamin J. Meyer <benmeyer@outlook.de>
2023-03-17 14:25:34 +0100

    Created some attack scripts / Only placeholder / functional = Graphical WIP!

--- Assets/Scripts/AI/UnitBehaviour/MiningAI.cs
+++ Assets/Scripts/AI/UnitBehaviour/MiningAI.cs
@@ -17,7 +17,7 @@ public class MiningAI : MonoBehaviour, IAIBehaviour
     public void UpdateState()
     {
         if (_unit.Dead) return;
-        if (_state == MiningState.GoingTo)
+        if (_state == MiningState.GoingTo && _agent.isOnNavMesh)
         {
             if (_agent.remainingDistance < _unit.UnitClass.MiningRange)
             {
--- Assets/Scripts/Attack/Attacks/TowerShoot.cs
+++ Assets/Scripts/Attack/Attacks/TowerShoot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class NormalShoot : Attack
+public class TowerShoot : Attack
 {
     private LineRenderer _lineRenderer;
     public override bool SpecificAttack(GameObject target)
