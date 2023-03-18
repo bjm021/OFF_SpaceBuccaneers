779ba7f66925689f1ee4fd01f8704cc748514095
Benjamin J. Meyer <benmeyer@outlook.de>
2023-03-18 17:17:55 +0100

    F

--- Assets/Scripts/Attack/Attacks/HarpoonAttack.cs
+++ Assets/Scripts/Attack/Attacks/HarpoonAttack.cs
@@ -29,7 +29,7 @@ public class HarpoonAttack : Attack
             if (!dead)
             {
                 Debug.Log("Stun");
-                unit.Stun(2.5);
+                unit.Stun(2.5f);
             }
             
             gameObject.GetComponent<SpecialPrioritisingAggressiveAI>().SearchNewTarget();
