c6afa67f09efe396e1cfa435cede72fb44678be1
Benjamin J. Meyer <benmeyer@outlook.de>
2023-03-18 14:42:04 +0100

    Multiplayer Data Fix

--- Assets/Scripts/Multiplayer/NetworkDebugManager.cs
+++ Assets/Scripts/Multiplayer/NetworkDebugManager.cs
@@ -1,10 +1,12 @@
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class NetworkDebugManager : MonoBehaviour
 {
     NetworkManager networkManager;
     private bool _host;
+    private UnityTransport _utp;
     private void Awake()
     {
         var networkDataCarrier = FindObjectOfType<NetworkDataCarrier>();
@@ -19,11 +21,16 @@ public class NetworkDebugManager : MonoBehaviour
         {
             _host = true;
             GameManager.Instance.Host = true;
+            _utp.ConnectionData.ServerListenAddress = networkDataCarrier.IP;
+            _utp.ConnectionData.Port = (ushort)networkDataCarrier.Port;
+            _utp.ConnectionData.Address = networkDataCarrier.IP;
         }
         else
         {
             _host = false;
             GameManager.Instance.Host = false;
+            _utp.ConnectionData.Address = networkDataCarrier.IP;
+            _utp.ConnectionData.Port = (ushort)networkDataCarrier.Port;
         }
     }
 
