08e88d7fdcb090d6c84648905bf799f54ce94acb
Benjamin J. Meyer <benmeyer@outlook.de>
2023-03-19 00:06:52 +0100

    Black Hole Bomber Fix

--- Assets/Scripts/Attack/Attacks/BlackHoleAttack.cs
+++ Assets/Scripts/Attack/Attacks/BlackHoleAttack.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -6,8 +7,15 @@ using UnityEngine;
 public class BlackHoleAttack : Attack
 {
     private LineRenderer _lineRenderer;
+
     public override bool SpecificAttack(GameObject target)
     {
+        // do nothing
+        return false;
+
+
+        /*
+        Debug.LogWarning("BlackHoleAttack");
         // Run attack on server / host / singleplayer
         VisualAttackRender(gameObject.transform.position, target.transform.position);
         // Run attack on clients
@@ -37,34 +45,89 @@ public class BlackHoleAttack : Attack
         }
         
         return false;
+        
+        */
     }
 
     [ClientRpc]
     public override void DrawOnClientRpc(Vector3 start, Vector3 end)
     {
-        if (GameManager.Instance.Host) return;
-        VisualAttackRender(start, end);
+        // Do nothing
     }
 
     private void VisualAttackRender(Vector3 start, Vector3 end)
     {
-        var beam = gameObject.AddComponent<LineRenderer>();
-        beam.startColor = Color.red;
-        beam.endColor = Color.red;
-        beam.startWidth = 0.1f;
-        beam.endWidth = 0.1f;
-        beam.positionCount = 2;
-        beam.SetPosition(0, start);
-        beam.SetPosition(1, end);
-        //beam.material = new Material(Shader.Find("Sprites/Default"));
-        beam.useWorldSpace = true;
-        StartCoroutine(DestroyBeam(beam));
+        // Do nothing
     }
-    
 
-    private IEnumerator DestroyBeam(LineRenderer beam)
+    private Coroutine _attackCoroutine;
+
+    private void Start()
     {
-        yield return new WaitForSeconds(0.3f);
-        Destroy(beam);
+        _attackCoroutine = StartCoroutine(AttackCoroutine(GetComponent<Unit>()));
+    }
+
+
+    private IEnumerator AttackCoroutine(Unit self)
+    {
+
+
+        while (true)
+        {
+            // Run attack on server / host / singleplayer
+            //VisualAttackRender(gameObject.transform.position, target.transform.position);
+            // Run attack on clients
+            //if (GameManager.Instance.Host) DrawOnClientRpc(gameObject.transform.position, target.transform.position);
+
+            //var targetUnit = target.GetComponent<Unit>();
+
+            if (self.Dead) yield break;
+            var targets = Physics.OverlapSphere(gameObject.transform.position, self.UnitClass.AttackRange,
+                LayerMask.GetMask("SpaceLaserLayer", "Mothership"));
+
+            foreach (var t in targets)
+            {
+                var parent = t.transform.parent;
+                if (parent.gameObject == gameObject) continue;
+                if (parent.TryGetComponent(out Mothership mothership))
+                {
+                    mothership.TakeDamage(Damage);
+                }
+                else
+                {
+                    var unit = parent.GetComponent<Unit>();
+
+                    if (parent.TryGetComponent(out Rigidbody rb))
+                    {
+                        rb.AddForce((transform.position - parent.transform.position).normalized * 10f, ForceMode.Impulse);
+                    }
+
+                    unit.TakeDamage(Damage);
+                }
+            }
+
+            yield return new WaitForSeconds(0.25f);
+
+            /*
+        var colliders = Physics.OverlapSphere(transform.position, self.UnitClass.AttackRange, LayerMask.GetMask("Unit"));
+        foreach (var c in colliders)
+        {
+            Debug.LogWarning("Colliding with " + c.name);
+            if (c.TryGetComponent(out Mothership mothership))
+            {
+                mothership.TakeDamage(Damage);
+            } 
+            else
+            { 
+                Debug.LogWarning("Dealing " + Damage + " damage to " + c.name);
+                var unit = c.GetComponent<Unit>();
+                unit.TakeDamage(Damage);
+            }
+        }
+
+        yield return new WaitForSeconds(0.25f);
+    }
+    */
+        }
     }
 }
