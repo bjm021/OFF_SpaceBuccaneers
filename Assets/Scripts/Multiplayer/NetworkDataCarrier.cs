77a547abb9b4436856906c8ba9abcfe25ef35d31
Benjamin J. Meyer <benmeyer@outlook.de>
2023-03-18 14:09:26 +0100

    Multiplayer Menu Woks!

---
+++ Assets/Scripts/Multiplayer/NetworkDataCarrier.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+
+public class NetworkDataCarrier : MonoBehaviour
+{
+    public string IP { get; set; }
+    public int Port { get; set; } = 7777;
+    public bool Host { get; set; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
