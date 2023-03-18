be3d29e85fb15c4c9eedf7db5a617c72726688f4
Pouvz <paul@meyer-michelstadt.de>
2023-03-19 00:09:25 +0100

    Ability indicators

---
+++ Assets/Scripts/AbilityIndicator.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityIndicator : MonoBehaviour
+{
+    [SerializeField] private AbilityClass abilityClass;
+    [SerializeField] private bool isMineField;
+    [SerializeField] private bool isEmp;
+    [SerializeField] private bool isBlackHole;
+    [SerializeField] private bool isLaser;
+    
+    private Camera _camera;
+    private Vector3 _laserPosition;
+
+    private void Awake()
+    {
+        if (isMineField)
+        {
+            transform.localScale = new Vector3(abilityClass.MineRadius, 0.5f, abilityClass.MineRadius);
+        }
+        else if (isEmp)
+        {
+            transform.localScale = new Vector3(abilityClass.EmpRange, 0.5f, abilityClass.EmpRange);
+        }
+        else if (isBlackHole)
+        {
+            transform.localScale = new Vector3(abilityClass.BlackHoleRange, 0.5f, abilityClass.BlackHoleRange);
+        }
+        else if (isLaser)
+        {
+            if (GameManager.Instance.Host)
+            {
+                _laserPosition = GameManager.Instance.PlayerOneMothership.transform.GetChild(0).position;
+            }
+            else
+            {
+                _laserPosition = GameManager.Instance.PlayerTwoMothership.transform.GetChild(0).position;
+            }
+            
+            transform.localScale = new Vector3(abilityClass.SpaceLaserWidth, 10, 1000);
+        }
+        
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (isLaser)
+        {
+            var worldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.y = 0;
+            
+            transform.position = _laserPosition;
+            transform.LookAt(worldPosition);
+        }
+    }
+}
